94e2d3ee6f1259a7ebdb6c84cc481c33d8fc1292
Steven Zhang <shsteven1000@GMail.com>
2014-12-04 22:35:39 +0800

    Initial Commit

---
+++ Shut Up/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shut Up
+//
+//  Created by Steven on 4/12/14.
+//  Copyright (c) 2014 Magical Bits. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Shut Up/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shut Up
+//
+//  Created by Steven on 4/12/14.
+//  Copyright (c) 2014 Magical Bits. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
