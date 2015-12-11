bf78e33092c6d5b4bf940277b389383d19a9fab8
Steven Zhang <shsteven1000@GMail.com>
2015-12-11 22:38:40 +0800

    move app to status item

--- Shut Up/AppDelegate.swift
+++ Shut Up/AppDelegate.swift
@@ -7,12 +7,27 @@
 //
 
 import Cocoa
+import PopStatusItem
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    let popStatusItem = PopStatusItem(image: NSImage(named: "buttonImage")!)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        
+        
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateControllerWithIdentifier("PopStatusItem") as? NSWindowController
+        popStatusItem.windowController = windowController
+        
+        popStatusItem.highlight = true // Default is false
+        popStatusItem.activate = true // Default is false
+        
+        popStatusItem.showPopover() // Show popover on startup
+
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
