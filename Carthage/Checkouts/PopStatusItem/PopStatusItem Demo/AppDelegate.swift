102f90d26f52d7335f5e2eec36752fa544a6ea7c
Steven Zhang <shsteven1000@GMail.com>
2015-12-11 22:26:37 +0800

    add cartfile

---
+++ Carthage/Checkouts/PopStatusItem/PopStatusItem Demo/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  PopStatusItem Demo
+//
+//  Created by Adam Hartford on 4/22/15.
+//  Copyright (c) 2015 Adam Hartford. All rights reserved.
+//
+
+import Cocoa
+import PopStatusItem
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let popStatusItem = PopStatusItem(image: NSImage(named: "buttonImage")!)
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        popStatusItem.windowController = storyboard.instantiateControllerWithIdentifier("PopStatusItem") as? NSWindowController
+        
+        popStatusItem.highlight = true // Default is false
+        popStatusItem.activate = true // Default is false
+        
+        popStatusItem.showPopover() // Show popover on startup
+        
+        // Force Aqua appearance, even in dark mode
+        //popStatusItem.popover.appearance = NSAppearance(named: NSAppearanceNameAqua)
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
